3c11cf146f67cf82b7135b9be5914092d84e2718
papamitsos93 <papamitsos93@gmail.com>
2021-06-07 17:12:57 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
+import threading
+from check_auth import maincheckauth
+# import processFrame
 
 if __name__ == '__main__':
-    print("heeeey")
-
+    print("[atlas-network-server] start")
+    try:
+        maincheckauth()
+        # check_auth()
+        # processFrame()
+    except Exception as e:
+        print(e)
