f882cb2ed83136e0760ce202742ef3c9fe8a51ed
paulBooz <82902650+paulBooz@users.noreply.github.com>
2021-06-25 12:39:15 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,13 +1,13 @@
 import requests
 from datetime import datetime, timedelta
-from utils.utils import normalize, fetch_data
+from utils.utils import makeAverage, fetch_data
 
 
 if __name__=="__main__":
-    startDate = datetime.today() - timedelta(hours=24)
+    startDate = datetime.today() - timedelta(hours=48)
     endDate = datetime.today()
-    data = fetch_data(1, startDate, endDate)  # data from the last 24 hours
-    normalized_values = normalize(data)
+    data = fetch_data(1, startDate, endDate)  # data from the last 48 hours
+    normalized_values = makeAverage(data, indexOfSensorId)
     model = tf.load_model("path/to/save/" + "_1_1") # analoga to onoma sto save apo to train
     predicted_values = model.predict(normalized_values, 12)  # next 12 values
     mongoDB.save(predicted_values)
