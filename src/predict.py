008844f39fba3dc71758ee0b3650516a393f11af
paulBooz <82902650+paulBooz@users.noreply.github.com>
2021-06-25 13:08:12 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,12 +1,13 @@
 import requests
 from datetime import datetime, timedelta
-from utils.utils import makeAverage, fetch_data
+from utils.utils import makeAverage, fetch_data, jsonToMatrix
 
 
 if __name__=="__main__":
     startDate = datetime.today() - timedelta(hours=48)
     endDate = datetime.today()
     data = fetch_data(1, startDate, endDate)  # data from the last 48 hours
+    data_matrix = jsonToMatrix(data)
     normalized_values = makeAverage(data, indexOfSensorId)
     model = tf.load_model("path/to/save/" + "_1_1") # analoga to onoma sto save apo to train
     predicted_values = model.predict(normalized_values, 12)  # next 12 values
