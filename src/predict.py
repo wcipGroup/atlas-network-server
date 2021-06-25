6d2f61dca77b8dc3234e3ff5c1298d86f099cc43
paulBooz <82902650+paulBooz@users.noreply.github.com>
2021-06-25 13:09:32 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -8,7 +8,7 @@ if __name__=="__main__":
     endDate = datetime.today()
     data = fetch_data(1, startDate, endDate)  # data from the last 48 hours
     data_matrix = jsonToMatrix(data)
-    normalized_values = makeAverage(data, indexOfSensorId)
+    data_hour = makeAverage(data_matrix, indexOfSensorId)
     model = tf.load_model("path/to/save/" + "_1_1") # analoga to onoma sto save apo to train
     predicted_values = model.predict(normalized_values, 12)  # next 12 values
     mongoDB.save(predicted_values)
